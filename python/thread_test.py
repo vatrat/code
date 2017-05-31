6c16d1ecc04a2993dd792ba1429624bd9715c8f0
vatrat <vatrat.dev@gmail.com>
2017-05-31 13:28:53 -0500

    Change formatting of thread_test.py

--- python/thread_test.py
+++ python/thread_test.py
@@ -1,13 +1,14 @@
-import time
+# import time
 import threading
 import logging
 import Queue
-#-----
-#Logging Setup
+
+
+# Logging Setup
 logging.basicConfig(level=logging.DEBUG,
-                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
-                    )
-#-----
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+
+
 def worker():
     logging.debug("start")
     while True:
@@ -15,10 +16,12 @@ def worker():
         logging.debug(a)
     logging.debug("stop")
 
+
 def q_reader(q, file):
     while True:
         file.write(q.get())
-#-----
+
+
 with open("test.txt", 'a') as file:
     q = Queue.Queue()
     t = threading.Thread(target=q_reader, args=(q, file))
